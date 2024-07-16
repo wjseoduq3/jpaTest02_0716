c6295a07ba6244a084921364026238ab3c02d1fd
wjseoduq <147117930+wjseoduq@users.noreply.github.com>
2024-07-16 13:02:02 +0900

    2'nd commit

--- src/main/java/com/jdy/jpashop/repository/MemberRepository.java
+++ src/main/java/com/jdy/jpashop/repository/MemberRepository.java
@@ -1,8 +1,8 @@
 package com.jdy.jpashop.repository;
 
 import com.jdy.jpashop.entity.Member;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,7 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     public List<Member> findByMembername(String membername);
     public List<Member> findByMembernameContaining(String membername);
     public List<Member> findByMembernameOrderByMemberageDesc(String membername);//회원 이름으로 검색 후 나이 내림차순 정렬
-    public List<Member> findAllbyOrderByMemberageDesc();
+    public List<Member> findAllByOrderByMemberageDesc();
 
     @Transactional
     public void deleteAllByMembername(String membername);
--- src/test/java/com/jdy/jpashop/JpashopTest.java
+++ src/test/java/com/jdy/jpashop/JpashopTest.java
@@ -24,7 +24,7 @@ public class JpashopTest {
     public void joinMember() {
 
         Member member = new Member();
-        member.setMemberid("gary4");
+        member.setMemberid("gary5");
         member.setMemberpw("1111");
         member.setMembername("이순신");
         member.setMemberage(55);
@@ -41,9 +41,7 @@ public class JpashopTest {
         assertTrue(memberOptional.isPresent()); // null 인지 아닌지 확인 가능
         Member member = memberOptional.get();
         System.out.println(member.getMembername());
-
     }
-
 
     @Test
     @DisplayName("회원이름으로 검색")
@@ -59,8 +57,8 @@ public class JpashopTest {
     @Test
     @DisplayName("모든 회원 검색")
     public void allMemberList() {
-        List<Member> alMemberList = memberRepository.findAll();
-        for (Member member:alMemberList) {
+        List<Member> allMemberList = memberRepository.findAll();
+        for (Member member:allMemberList) {
             System.out.print(member.getMembernum() + " / ");
             System.out.print(member.getMemberid() + " / ");
             System.out.print(member.getMembername() + " / ");
@@ -90,28 +88,24 @@ public class JpashopTest {
             System.out.print(member.getMembername() + " / ");
             System.out.println(member.getMemberage() + " / ");
         }
-
     }
-
-
 
     @Test
     @DisplayName("회원목록 검색 후 나이의 내림차순 정렬")
     public void memberSearch2() {
-        List<Member> memberList = memberRepository.findAllbyOrderByMemberageDesc();
+        List<Member> memberList = memberRepository.findAllByOrderByMemberageDesc();
         for (Member member : memberList) {
             System.out.print(member.getMembernum() + " / ");
             System.out.print(member.getMemberid() + " / ");
             System.out.print(member.getMembername() + " / ");
             System.out.println(member.getMemberage() + " / ");
         }
-
     }
 
     @Test
     @DisplayName("회원정보 수정")
     public void modifyMember() {
-        Optional<Member> memberOptional = memberRepository.findById(2L);
+        Optional<Member> memberOptional = memberRepository.findById(4L);
 
         assertTrue(memberOptional.isPresent());
 
@@ -119,16 +113,14 @@ public class JpashopTest {
         member.setMembername("강감찬");
         Member savedMember = memberRepository.save(member);
         System.out.println(savedMember.toString());
-
     }
 
     @Test
     @DisplayName("회원번호로 삭제")
     public void deleteMember() {
         allMemberList();
-        memberRepository.deleteById(4L);
+        memberRepository.deleteById(5L);
         allMemberList();
-
     }
 
     @Test
@@ -137,7 +129,6 @@ public class JpashopTest {
         allMemberList();
         memberRepository.deleteAllByMembername("계백");
         allMemberList();
-
     }
 
 }
